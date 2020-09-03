004633662a422646a43886700e826c0e6fb54596
Kiran Ramesh <kiran.rs2@gmail.com>
2020-09-03 03:59:40 -0400

    Bootstrap and MongoDB remote connection

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
+// Load modules
 const express = require('express');
 const expbhs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+// Connect to MongoURI exported from config
+const keys = require('./config/keys');
+
+// Initialize application
 const app = express();
+
+// Template engine for view 
 app.engine('handlebars',expbhs({defaultLayout: 'main'}));
-
 app.set('view engine', 'handlebars');
-const port = 3000;
 
+// Setup static files to store css, js, images
+app.use(express.static('public'));
+
+//connect to remote db using keys imported above
+mongoose.connect(keys.MongoURI, {useNewUrlParser: true})
+.then(() => {console.log('Connected to remote db successfully')}).catch(() =>
+{console.log(err)});
+
+// server port set as env variable and for local port as 3000
+const port = process.env.PORT || 3000;
+
+
+// route for pages
 app.get('/',(req,res)=>{
     res.render('home')});
 
 app.get('/about',(req,res)=>{
-    res.send('About');
+    res.render('about');
 });
 
+//success message if server is running
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
