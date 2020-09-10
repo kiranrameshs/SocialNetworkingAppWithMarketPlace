6492a74799f4fbd178548d0fac9f27e7443a640c
Kiran Ramesh <kiran.rs2@gmail.com>
2020-09-10 17:25:06 -0400

    Added Config Vars - Heroku

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI:'mongodb+srv://socialnetworkingapp:sna123@socialnetworkingapp.e90qu.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '445179052641-bhn9rdc2ee8aggshnk13i49l32q4v3me.apps.googleusercontent.com',
-    GoogleSecret: 'QEiQ0mgB9tosYf0sGdKF0AvK'
+    MongoURI:process.env.Mongo_URI,
+    GoogleClientID: process.env.Google_ClientID,
+    GoogleSecret: process.env.Google_Secret
 }
